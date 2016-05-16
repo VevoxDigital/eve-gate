94fa9e889837151826bcfee86153560ccdb9a22c
Matthew Struble <matt@vevox.io>
2016-05-15 20:18:05 -0700

    Navbar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,5 +30,115 @@ angular
       });
   })
   .run(function ($rootScope) {
-    
+    $rootScope.$meta = {
+      title: function (title) {
+
+      },
+      nav: [
+        [
+          {
+            text: 'Database',
+            href: 'database',
+            items: [
+              {
+                text: 'Wormhole Classes',
+                href: 'wormhole'
+              },
+              {
+                text: 'Cosmic Signatures',
+                href: 'sigs'
+              },
+              {},
+              {
+                text: 'Item Database',
+                href: 'items'
+              }
+            ]
+          },
+          {
+            text: 'Logistics',
+            href: 'logi',
+            items: [
+              {
+                text: 'Wormhole Mapping',
+                href: 'wormhole'
+              },
+              {
+                text: 'System Lookup',
+                href: 'system'
+              },
+              {},
+              {
+                text: 'Ship/Citadel Fitting',
+                href: 'ships'
+              },
+              {
+                text: 'Starbase Fitting',
+                href: 'pos'
+              }
+            ]
+          },
+          {
+            text: 'Industry',
+            href: 'industry',
+            items: [
+              {
+                text: 'Mining Statistics',
+                href: 'mining'
+              },
+              {
+                text: 'Reactions Statistics',
+                href: 'reactions'
+              }
+            ]
+          },
+          {
+            text: 'The Gate',
+            href: 'gate',
+            items: [
+              {
+                text: 'Support Us',
+                href: 'support'
+              },
+              {
+                text: 'GitHub Page',
+                href: '@https://github.com/VevoxDigital/eve-gate'
+              },
+              {},
+              {
+                text: 'The Team',
+                href: 'team'
+              }
+            ]
+          }
+        ],
+        [
+
+        ]
+      ]
+    };
+
+    $rootScope.toggleNavDD = function (e, data) {
+      var dd = angular.element('#navDD'), link = angular.element(e.target);
+      if (!data) {
+        dd.hide();
+      } else {
+        if (!data.items) return;
+        link.append(dd);
+        dd.show();
+        dd.empty();
+        data.items.forEach(function (item) {
+          if (item.text) {
+            if (item.href.startsWith('@')) {
+              dd.append('<a href="' + item.href.substring(1) + '" target="_blank">' + item.text + '</a>');
+            } else {
+              dd.append('<a href="/' + data.href + '/' + item.href + '">' + item.text + '</a>');
+            }
+          } else {
+            dd.append('<hr>');
+          }
+        });
+      }
+    };
+
   });
