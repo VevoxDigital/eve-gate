2cdc62a3f310ba8fe8f0ffb574bf3414cbb1c6bf
Matthew Struble <matt@vevox.io>
2016-05-16 19:08:56 -0700

    VelocityJS

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,8 @@ module.exports = function(config) {
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/velocity/velocity.js',
+      'bower_components/velocity/velocity.ui.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
