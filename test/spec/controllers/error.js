36f8d84c1eb2f82fc50bbb0dbe7821314c3f16f7
Matthew Struble <matt@vevox.io>
2016-05-27 15:42:28 -0700

    Placeholder spec test for error page

---
+++ test/spec/controllers/error.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: ErrorCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('eveGateApp'));
+
+  var ErrorCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    ErrorCtrl = $controller('ErrorCtrl', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  it('do something', function () {
+    expect(ErrorCtrl).toBe(true);
+  });
+});
