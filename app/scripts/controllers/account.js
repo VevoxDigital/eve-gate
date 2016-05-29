3ba141362553b1e9dfceee05b0d437e63925a8be
Matthew Struble <matt@vevox.io>
2016-05-29 14:31:43 -0700

    Error popups working

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -9,10 +9,18 @@
  */
 angular.module('eveGateApp')
   .controller('AccountCtrl', function ($scope, $user, $timeout) {
-    $scope.loginBox = {};
+    $scope.login = {};
+    $scope.$watch('login.email', function () { $scope.pushError(); });
     $scope.submitLogin = function () {
-      $user.logging = true;
       $scope.pushError();
+      var data = $scope.login;
+      if (!data.email || !data.email.match(/^[^@]+@.+\..+$/)) { return $scope.pushError(0, 'That doesn\'t look quite right.'); }
+      if (!data.pass) { return $scope.pushError(1, 'You\'ll need a password for this.'); }
+      if (data.new) {
+        if (data.pass !== data.pass2) { return $scope.pushError(2, 'These don\'t seem to match.'); }
+        if (!data.tos) { return $scope.pushError(3, 'We\'ll need you to accept this.'); }
+      }
+      $user.logging = true;
       $timeout(function () {
         $user.logging = false;
         $timeout(function () {
@@ -21,15 +29,15 @@ angular.module('eveGateApp')
       }, 1000);
     };
     $scope.pushError = function (eid, err) {
-      var popup = angular.element('#errorPopup'), e = angular.element('[vx-eid='+eid+']');;
+      var popup = angular.element('#errorPopup');
       if (!eid && eid !== 0) {
-        e.removeClass('error');
+        popup.parents('form').find('.error').removeClass('error');
         delete $scope.loginError;
       } else {
+        var e = angular.element('[vx-eid='+eid+']');
         if (e.length === 0) { e = angular.element('[vx-eid=4]'); }
         popup.detach().appendTo(e.addClass('error').parent());
         $scope.loginError = err;
       }
     };
-    $scope.$watch('loginBox.checkNew', function() { $scope.pushError(); });
   });
