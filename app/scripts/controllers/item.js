073bdff0a35c6197642879cad877bdcc4ec2053e
Matthew Struble <matt@vevox.io>
2016-06-12 10:58:45 -0700

    Finished item searching

--- app/scripts/controllers/item.js
+++ app/scripts/controllers/item.js
@@ -8,7 +8,39 @@
  * Controller of the tech3App
  */
 angular.module('tech3App')
-  .controller('ItemCtrl', function ($routeParams) {
-    this.itemID = $routeParams.item;
-    this.itemSearch = {};
+  .controller('ItemCtrl', function ($scope, $routeParams, itemService) {
+    $scope.itemID = $routeParams.item;
+    $scope.itemSearch = { showDust: false, showBP: false };
+
+    var results;
+    $scope.data = 'Enter at least 4 characters to start searching.';
+
+    $scope.dataDisplayable = function () {
+      return typeof $scope.data === typeof [];
+    };
+
+    $scope.$watch('itemSearch.name', function (val) {
+      if (val && val.length >= 4) {
+        if (results) {
+          if (typeof results === typeof []) {
+            $scope.data = [];
+            results.forEach(function (res) {
+              if (res.name.toLowerCase().includes(val.toLowerCase())) {
+                $scope.data.push(res);
+              }
+            });
+          } else {
+            $scope.data = results;
+          }
+        } else {
+          itemService.search(val, function (res) {
+            results = res;
+            $scope.data = results;
+          });
+        }
+      } else {
+        $scope.data = 'Enter at least 4 characters to start searching.';
+        results = null;
+      }
+    });
   });
---
+++ app/scripts/services/itemservice.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tech3App.itemService
+ * @description
+ * # itemService
+ * Service in the tech3App.
+ */
+angular.module('tech3App')
+  .service('itemService', function ($http) {
+    this.search = function (search, cb) {
+      $http({
+        method: 'GET',
+        //url: '/api/type',
+        url: 'http://192.168.0.102:3000/api/type',
+        params: { name: search }
+      }).then(function (res) {
+        cb(res.data);
+      }, function (res) {
+        cb(res.status + ': ' + res.data);
+      });
+    };
+  });
