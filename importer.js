aa8a46868c12f0199111507caa255cc3c3e4045d
Matthew Struble <matt@vevox.io>
2016-06-14 19:18:53 -0700

    Importer now imports some dogma

--- importer.js
+++ importer.js
@@ -17,42 +17,75 @@ console.log('Attempting to connect to the database...');
 const dbURL = 'mongodb://' + config.get('db:host') + ':' + config.get('db:port') + '/' + config.get('db:db');
 mongoose.connect(dbURL, {}, () => {
 
-  var Item = mongoose.model('Item', require('./lib/schema/item'));
+  var Item          = mongoose.model('Type', require('./lib/schema/item')),
+      DogmaTypeAttr = mongoose.model('DogmaTypeAttr', require('./lib/schema/dogmaTypeAttr'));
 
   try {
-    async.eachSeries([ (done) => {
-      console.log('Parsing items file...');
-      var itemFile = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'sde', 'typeIDs.yaml'))), items = [];
-      _.forEach(itemFile, (data, id) => {
-        data.id = parseInt(id);
-        items.push(data);
-      });
+    async.eachSeries([
+      (done) => {
+        console.log('Parsing items file...');
+        var itemFile = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'sde', 'fsd', 'typeIDs.yaml'))), items = [];
+        _.forEach(itemFile, (data, id) => {
+          data.id = parseInt(id);
+          items.push(data);
+        });
 
-      var c = 1;
-      async.eachSeries(items, (item, cb) => {
-        process.stdout.clearLine();
-        process.stdout.cursorTo(0);
-        process.stdout.write(' * Importing item ' + c++ + ' of ' + items.length);
-        var entry = new Item({
-          _id: item.id,
-          name: item.name.en || 'Unknown',
-          published: item.published,
-          group: item.groupID,
-          meta: {
-            mass: item.mass,
-            volume: item.volume,
-            radius: item.radius
-          }
-        });
-        Item.find({ _id: item.id }).remove((err) => {
-          if (err) throw err;
-          entry.save((err) => {
-            if (err) throw new Error(err + '\n' + entry.name + '#' + entry._id);
-            cb();
+        var c = 1;
+        async.eachSeries(items, (item, cb) => {
+          process.stdout.clearLine();
+          process.stdout.cursorTo(0);
+          process.stdout.write(' * Importing item ' + c++ + ' of ' + items.length);
+          var entry = new Item({
+            _id: item.id,
+            name: item.name.en || 'Unknown',
+            published: item.published,
+            group: item.groupID,
+            meta: {
+              mass: item.mass,
+              volume: item.volume,
+              radius: item.radius
+            }
           });
-        });
-      }, () => { console.log(); done(); });
-    }], (func, cb) => { func(cb); }, () => {
+          Item.find({ _id: item.id }).remove((err) => {
+            if (err) throw err;
+            entry.save((err) => {
+              if (err) throw new Error(err + '\n' + entry.name + '#' + entry._id);
+              cb();
+            });
+          });
+        }, () => { console.log(); done(); });
+      },
+      (done) => {
+        console.log('Parsing attributes file...');
+        var dgmAttributeTypesYAML = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'sde', 'bsd', 'dgmAttributeTypes.yaml')));
+
+        var c = 1;
+        async.eachSeries(dgmAttributeTypesYAML, (attr, cb) => {
+          process.stdout.clearLine();
+          process.stdout.cursorTo(0);
+          process.stdout.write(' * Importing attribute ' + c++ + ' of ' + dgmAttributeTypesYAML.length);
+          var entry = new DogmaTypeAttr({
+            _id: attr.attributeID,
+            name: attr.attributeName,
+            category: attr.categoryID,
+            published: attr.published,
+            meta: {
+              displayName: attr.displayName,
+              stackable: attr.stackable,
+              highIsGood: attr.highIsGood,
+              description: attr.description
+            }
+          });
+          DogmaTypeAttr.find({ _id: attr.attributeID }).remove((err) => {
+            if (err) throw err;
+            entry.save((err) => {
+              if (err) throw new Error(err + '\n' + entry.name + '#' + entry._id);
+              cb();
+            });
+          });
+        }, () => { console.log(); done(); });
+      }
+    ], (func, cb) => { func(cb); }, () => {
       console.log('Done!');
       process.exit(0);
     });
