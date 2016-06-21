89697a48888cede388334cafcbabf34c3c12cf27
Matthew Struble <matt@vevox.io>
2016-06-21 10:26:00 -0700

    Finished search dropdown

--- app/scripts/controllers/database-search.js
+++ app/scripts/controllers/database-search.js
@@ -9,15 +9,21 @@
  */
 angular.module('tech3App')
   .controller('DatabaseSearchCtrl', function ($scope) {
-    $scope.searchOptions = [ ['Items', 'type'] ];
+    $scope.searchOptions = [
+      ['Items', 'type', '/info/type'],
+      ['Celestials', 'celestial', '/info/celestial'],
+      ['Signatures', 'sig', '/info/sig'],
+      ['Systems', 'system', '/logi/starmap']
+    ];
     $scope.search = { opt: 0 };
 
     var searchOptsE = angular.element('#dbSearchOpts');
     var searchOptsButtonE = searchOptsE.children('button'),
-        searchOptsDDOpt = searchOptsE.children('a');
+        searchOptsDDOpt = searchOptsE.children('.form-dropdown-content');
     searchOptsButtonE.click(function () { searchOptsE.toggleClass('shown'); });
     searchOptsDDOpt.click(function (event) {
       searchOptsE.removeClass('shown');
       $scope.search.opt = parseInt(angular.element(event.target).attr('vx-search-opt'));
+      $scope.$apply();
     });
   });
