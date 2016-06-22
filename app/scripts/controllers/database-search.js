3d92606fd4b68dd8c72c0bd91e2966c9d83ea561
Matthew Struble <matt@vevox.io>
2016-06-21 21:05:43 -0700

    Type search function

--- app/scripts/controllers/database-search.js
+++ app/scripts/controllers/database-search.js
@@ -8,7 +8,7 @@
  * Controller of the tech3App
  */
 angular.module('tech3App')
-  .controller('DatabaseSearchCtrl', function ($scope) {
+  .controller('DatabaseSearchCtrl', function ($scope, backendService) {
     $scope.searchOptions = [
       ['Items', 'type', '/info/type'],
       ['Celestials', 'celestial', '/info/celestial'],
@@ -26,4 +26,47 @@ angular.module('tech3App')
       $scope.search.opt = parseInt(angular.element(event.target).attr('vx-search-opt'));
       $scope.$apply();
     });
+
+    // Search functionals.
+    var MIN_CHARS = 4, results, lastOpt = $scope.search.opt;
+    $scope.showResults = function () { return typeof $scope.searchData === typeof { } };
+
+    var searchDataUpdate = function (query) {
+      if (query && query.length >= 4) {
+        if (results && lastOpt == $scope.search.opt) {
+          if (typeof results === typeof []) {
+            $scope.searchData = [];
+            results.forEach(function (res) {
+              if (res.name.toLowerCase().includes(query.toLowerCase())) {
+                $scope.searchData.push(res);
+              }
+            });
+          } else {
+            $scope.searchData = results;
+          }
+        } else {
+          $scope.searchData = '<i class="fa fa-circle-o-notch fa-spin"></i>';
+          backendService.get($scope.searchOptions[$scope.search.opt][1], { query: query }, function (res) {
+            switch (res.status) {
+              case 200:
+                results = res.data;
+                break;
+              case -1:
+                results = 'An unknown error has occurred.<br>See your browser\'s console for additional information.';
+                break;
+              default:
+                results = 'Error ' + res.status + ' during search: ' + JSON.stringify(res.data);
+                break;
+            }
+            $scope.searchData = results;
+          });
+        }
+      } else {
+        $scope.searchData = 'Enter at least ' + MIN_CHARS + ' to begin searching.';
+        results = null;
+      }
+    };
+    $scope.$watch('search.name', searchDataUpdate);
+    $scope.$watch('search.opt', function () { results = null; searchDataUpdate($scope.search.name); });
+    searchDataUpdate();
   });
