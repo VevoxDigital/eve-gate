964f939932f485ebd420c3fbf059b5ed10b76c1e
Matthew Struble <matt@vevox.io>
2016-07-03 19:48:29 -0700

    Added appraisal query UI

--- app/scripts/controllers/market.js
+++ app/scripts/controllers/market.js
@@ -8,6 +8,17 @@
  * Controller of the tech3App
  */
 angular.module('tech3App')
-  .controller('MarketCtrl', function () {
-
+  .controller('MarketCtrl', function ($scope) {
+    $scope.searchQuery = [];
+    $scope.query = { };
+    $scope.addQuery = function () {
+      if (!$scope.query.name) { return; }
+      if (!$scope.query.quantity) { $scope.query.quantity = 1; }
+      $scope.searchQuery.push({ name: $scope.query.name, num: $scope.query.quantity });
+      $scope.query = { };
+      angular.element('.query-adder input[name=name]').focus();
+    };
+    $scope.delQuery = function (index) {
+      $scope.searchQuery.splice(index, 1);
+    };
   });
