9dfdef4ceb351369b5fe944123a3eb52c880f978
Matthew Struble <matt@vevox.io>
2016-07-05 23:30:25 -0700

    Styles and totals for appraisal

--- app/scripts/controllers/market.js
+++ app/scripts/controllers/market.js
@@ -11,9 +11,10 @@ angular.module('tech3App')
   .controller('MarketCtrl', function ($scope, backendService) {
     $scope.searchQuery = [];
     $scope.appraisal = []; $scope.appraisalError = null;
+    $scope.appraisalTotal = { volume: 0, price: 0 };
     $scope.query = { }; $scope.station = '';
     $scope.addQuery = function () {
-      if (!$scope.query.name) { return; }
+      if (!$scope.query.name) { return $scope.createAppraisal(); }
       if (!$scope.query.quantity) { $scope.query.quantity = 1; }
       $scope.searchQuery.push({ name: $scope.query.name, num: $scope.query.quantity });
       $scope.query = { };
@@ -32,11 +33,16 @@ angular.module('tech3App')
         method: 'POST'
       }, function (res) {
         if (res.status === 200) {
-          function commas(x) {
+          var commas = function (x) {
             return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-          }
+          };
           $scope.appraisal = [];
+          $scope.appraisalTotal = { volume: 0 };
           res.data.forEach((item) => {
+            if (!$scope.appraisalTotal.price) {
+              $scope.appraisalTotal.price = item.price.buy ? { buy: 0, sell: 0 } : 0;
+            }
+
             var price = item.price.buy ? {
               buy: Math.floor(item.price.buy * 100) / 100,
               sell: Math.floor(item.price.sell * 100) / 100
@@ -45,9 +51,17 @@ angular.module('tech3App')
               buy: Math.floor((price.buy/item.volume) * 100) / 100,
               sell: Math.floor((price.sell/item.volume) * 100) / 100
             } : Math.floor((price/item.volume) * 100) / 100;
+
+            $scope.appraisalTotal.volume += item.volume * item.quantity;
+            $scope.appraisalTotal.price = item.price.buy ? {
+              buy: $scope.appraisalTotal.price.buy + (price.buy * item.quantity),
+              sell: $scope.appraisalTotal.price.sell + (price.sell * item.quantity)
+            } : $scope.appraisalTotal.price + (price * item.quantity);
+
             $scope.appraisal.push({
               name: item.name,
-              volume: item.volume,
+              err: item.err,
+              volume: commas(item.volume),
               quantity: item.quantity,
               price: item.price.buy ? {
                 buy: {
@@ -56,7 +70,7 @@ angular.module('tech3App')
                   m3: ppv.buy,
                   m3_str: commas(ppv.buy),
                   total: price.buy * item.quantity,
-                  total_str: commans(price.buy * item.quantity)
+                  total_str: commas(price.buy * item.quantity)
                 },
                 sell: {
                   unit: price.sell,
@@ -76,6 +90,11 @@ angular.module('tech3App')
               }
             });
           });
+          $scope.appraisalTotal.volume = commas($scope.appraisalTotal.volume);
+          $scope.appraisalTotal.price = $scope.appraisalTotal.price.buy ? {
+            buy: commas($scope.appraisalTotal.price.buy),
+            sell: commas($scope.appraisalTotal.price.sell)
+          } : commas($scope.appraisalTotal.price);
         } else {
           $scope.appraisalError = res.data ? res.status + ' Unknown error' : res.data.message;
         }
