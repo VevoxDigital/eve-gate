e79952dfccb6795042c77366277c1918f29ec807
Matthew Struble <matt@vevox.io>
2016-07-23 12:29:46 -0700

    Moved nav-link template

--- app/scripts/directives/nav-link.js
+++ app/scripts/directives/nav-link.js
@@ -9,7 +9,7 @@
 angular.module('tech3App')
   .directive('navLink', function () {
     return {
-      templateUrl: '/views/directives/nav-link.html',
+      templateUrl: 'views/tpl/nav-link.html',
       restrict: 'A',
       scope: { navLinkScope: '&' },
       link: function (scope) {
